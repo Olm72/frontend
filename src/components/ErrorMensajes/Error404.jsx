2f3bffabfa069d6a4a8e4dfacabe9694f63bd3d2
Olm72 <olm22fr@gmail.com>
2024-12-18 13:37:46 +0100

    Cambio de ErrorMensajes.css de carpeta components a styles

--- src/components/ErrorMensajes/Error404.jsx
+++ src/components/ErrorMensajes/Error404.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
-import "./ErrorMensajes.css";
+import "../../styles/ErrorMensajes.css";
 import { Link } from "react-router-dom";
 
 const Error404 = () => {
