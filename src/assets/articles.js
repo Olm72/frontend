dbe7770e02cc2e3f54c9c5506d3448efff5cb4e5
angman <licangelmanrique@gmail.com>
2024-12-22 02:42:45 +0100

    archivos añadidos

---
+++ src/assets/articles.js
@@ -0,0 +1,57 @@
+const articulos = [
+    {
+      id: 1,
+      titulo: "El Impacto del Big Data en la Toma de Decisiones Empresariales",
+      extracto:
+        "En un mundo cada vez más impulsado por los datos, el Big Data se ha convertido en un factor crucial para la toma de decisiones estratégicas en las empresas. En Nexeus Big Data, ofrecemos a nuestros clientes la capacidad de transformar grandes volúmenes de datos en información accionable, ayudándoles a mantenerse a la vanguardia de su sector. Lee más sobre la importancia del Big Data en la toma de decisiones. Explora cómo nuestras soluciones pueden beneficiar a tu negocio.",
+      fuente: "Forbes",
+      fecha: "20/07/2024",
+      enlace:
+        "https://www.forbes.com/sites/bernardmarr/2022/02/07/the-importance-of-data-driven-decision-making-in-business/?sh=37c4e5fe1a76",
+    },
+    {
+      id: 2,
+      titulo:
+        "Cómo la Inteligencia Artificial Está Transformando el Sector de Negocios",
+      extracto:
+        "La Inteligencia Artificial (IA) está cambiando radicalmente la forma en que las empresas operan. En Nexeus Big Data, estamos a la vanguardia de esta transformación, ofreciendo soluciones de IA que mejoran la eficiencia, reducen costos y permiten a las empresas tomar decisiones más informadas. Lee más sobre cómo la IA está impactando el sector empresarial. Explora cómo nuestras soluciones pueden beneficiar a tu negocio.",
+      fuente: "Boston Consulting Group",
+      fecha: "16/07/2024",
+      enlace:
+        "https://www.bcg.com/publications/2023/how-ai-is-transforming-businesses",
+    },
+    {
+      id: 3,
+      titulo:
+        "Visualización de Datos: Potenciando la Comprensión de Información Compleja",
+      extracto:
+        "La visualización de datos es esencial para convertir grandes volúmenes de información en insights comprensibles. En Nexeus Big Data, nuestros expertos crean visualizaciones que permiten a las empresas interpretar datos complejos de manera rápida y efectiva.Para más información sobre la importancia de la visualización de datos, visita este artículo de Tableau.Descubre cómo nuestras soluciones de visualización de datos pueden ayudarte a entender mejor tu negocio.",
+      fuente: "Tableau",
+      fecha: "13/07/2024",
+      enlace: "https://www.tableau.com/visualization/what-is-data-visualization",
+    },
+    {
+      id: 4,
+      titulo:
+        "Business Intelligence: Transformando Datos en Estrategias Empresariales",
+      extracto:
+        "El Business Intelligence (BI) es clave para transformar datos en estrategias empresariales efectivas. En Nexeus Big Data, proporcionamos soluciones de BI que permiten a nuestros clientes convertir sus datos en un recurso estratégico para el éxito a largo plazo. Explora el poder del Business Intelligence en el mundo empresarial. Explora cómo nuestras soluciones pueden beneficiar a tu negocio.",
+      fuente: "Gartner",
+      fecha: "23/06/2024",
+      enlace:
+        "https://www.gartner.com/en/information-technology/glossary/business-intelligence-bi",
+    },
+    {
+      id: 5,
+      titulo:
+        "Chatbots Inteligentes: Mejorando la Experiencia del Cliente con IA",
+      extracto:
+        "Los chatbots impulsados por Inteligencia Artificial están revolucionando la atención al cliente. En Nexeus Big Data, desarrollamos chatbots que no solo responden consultas, sino que también anticipan las necesidades de los clientes, mejorando la experiencia de usuario. Lee más sobre el impacto de los chatbots en la atención al cliente. Explora cómo nuestras soluciones pueden beneficiar a tu negocio.",
+      fuente: "Gartner",
+      fecha: "11/06/2024",
+      enlace:
+        "https://www.gartner.com/en/information-technology/glossary/business-intelligence-bi",
+    },
+  ];
+  
+  export default articulos;
